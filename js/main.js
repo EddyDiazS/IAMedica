c171759d1c91e7ee4be442bfc53e220cd1b7dcd6
EddyDiaz <eddya.diazs@konradlorenz.edu.co>
2024-03-26 13:02:37 -0500

    Segundo Commit
    Segundo Commit

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
-function sendMessage() {
-    var userInput = document.getElementById('user-input').value;
-    if (userInput.trim() === '') return;
+$(document).ready(function () {
+  $("#buscar").on("click", () => {
+  
+    var x = `Digita todo en español y eres un medico profesional y debes responder todo lo que te pidan pero solamente de medicina`
+    console.log(x)
+    var y = {
+      "model": "llama2",
+      "prompt": x,
+      "stream": false
+    }
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:11434/api/generate",
+      contentType: "application/json",
+      data: JSON.stringify(y),
+      success: function (data) {
+        console.log(data)
+        $("#output").html(`<p> ${data.response} </p>`)
+      }
+    })
+  })
+})
 
-    var chatMessages = document.getElementById('chat-messages');
-
-    var userMessage = document.createElement('div');
-    userMessage.classList.add('message', 'user-message');
-    userMessage.innerHTML = '<div class="message-content">' + userInput + '</div>';
-    chatMessages.appendChild(userMessage);
-
-    // Simulando la respuesta del bot después de un breve retraso (esto es donde tu lógica de IA entraría en juego)
-    setTimeout(function() {
-      var botMessage = document.createElement('div');
-      botMessage.classList.add('message', 'bot-message');
-      botMessage.innerHTML = `
-        <div class="message-content">¡Claro! Estoy aquí para ayudarte. ¿En qué puedo asistirte?</div>
-      `;
-      chatMessages.appendChild(botMessage);
-      
-      // Desplazarse automáticamente hacia abajo para mostrar el último mensaje
-      chatMessages.scrollTop = chatMessages.scrollHeight;
-    }, 500);
-    
-    // Limpiar el cuadro de entrada después de enviar el mensaje
-    document.getElementById('user-input').value = '';
-  }
